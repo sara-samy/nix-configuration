e6a2f5ba054684c3bf014bcb2ab576cfaa999ade
sara-samy <intermsofart@hotmail.com>
2024-07-07 18:08:11 +0200

    nixos: refactor nixos users

--- hosts/nixos/users.nix
+++ hosts/nixos/users.nix
@@ -22,8 +22,8 @@
   };
 
   # Enable automatic login for the user
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "${username}";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "${username}";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
