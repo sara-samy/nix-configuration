2674e5847a1138c6f1cc7b65ca65914c0e9e1dd0
sara-samy <intermsofart@hotmail.com>
2025-04-19 18:11:27 +0200

    shared: switch to 24.11 stable release

--- flake.nix
+++ flake.nix
@@ -3,16 +3,16 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      url = "github:NixOS/nixpkgs/nixos-24.11";
     };
 
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
