e95578d8e946555f18d27ddb2b02ecdc11edaf96
sara-samy <intermsofart@hotmail.com>
2025-04-19 18:09:55 +0200

    darwin: disable nix (managed by determinate systems)

--- hosts/darwin/core.nix
+++ hosts/darwin/core.nix
@@ -4,13 +4,7 @@
   username,
   ...
 }: {
-  # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = nixpkgs;
-
-  nix.settings = {
-    auto-optimise-store = false;
-    experimental-features = ["nix-command" "flakes"];
-  };
+  nix.enable = false;
 
   # Allow unfree packages
   nixpkgs.config = {
@@ -18,14 +12,4 @@
     allowBroken = false;
     allowUnsupportedSystem = false;
   };
-
-  # Garbage collect weekly
-  nix.gc = {
-    user = username;
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = false;
 }
